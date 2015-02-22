42a4c24e4abc9a5ad08874ba2acf72efa907bb2e
Andrew Millman <andrewmillman35@gmail.com>
2015-02-21 17:17:25 -0800

    Add Badge Model

---
+++ KABadges/KABadges/KABBadge.m
@@ -0,0 +1,13 @@
+//
+//  KABBadge.m
+//  KABadges
+//
+//  Created by Andrew on 2/21/15.
+//  Copyright (c) 2015 Andrew Millman. All rights reserved.
+//
+
+#import "KABBadge.h"
+
+@implementation KABBadge
+
+@end
