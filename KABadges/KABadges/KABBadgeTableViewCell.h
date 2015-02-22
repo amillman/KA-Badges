eb49e1357c670d74485120d0fc9b81d939fff54d
Andrew Millman <andrewmillman35@gmail.com>
2015-02-21 20:38:04 -0800

    Add KABTableViewCell and split files into groups

---
+++ KABadges/KABadges/KABBadgeTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  KABBadgeTableViewCell.h
+//  KABadges
+//
+//  Created by Andrew on 2/21/15.
+//  Copyright (c) 2015 Andrew Millman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KABBadgeTableViewCell : UITableViewCell
+
+@end
---
+++ KABadges/KABadges/KABBadgeTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  KABBadgeTableViewCell.m
+//  KABadges
+//
+//  Created by Andrew on 2/21/15.
+//  Copyright (c) 2015 Andrew Millman. All rights reserved.
+//
+
+#import "KABBadgeTableViewCell.h"
+
+@implementation KABBadgeTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
