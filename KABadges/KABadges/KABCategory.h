76c5bc53a508149bb025e910b9fcc0e5b316d785
Andrew Millman <andrewmillman35@gmail.com>
2015-02-21 20:06:15 -0800

    Add category model

---
+++ KABadges/KABadges/KABCategory.h
@@ -0,0 +1,19 @@
+//
+//  KABCategory.h
+//  KABadges
+//
+//  Created by Andrew on 2/21/15.
+//  Copyright (c) 2015 Andrew Millman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KABCategory : NSObject
+
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *details;
+@property (strong, nonatomic) NSNumber *categoryNumber;
+@property (strong, nonatomic) NSURL *smallIconURL;
+@property (strong, nonatomic) NSURL *largeIconURL;
+
+@end
---
+++ KABadges/KABadges/KABCategory.m
@@ -0,0 +1,13 @@
+//
+//  KABCategory.m
+//  KABadges
+//
+//  Created by Andrew on 2/21/15.
+//  Copyright (c) 2015 Andrew Millman. All rights reserved.
+//
+
+#import "KABCategory.h"
+
+@implementation KABCategory
+
+@end
