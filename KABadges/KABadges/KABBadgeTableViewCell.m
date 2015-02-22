ba792488897e042da7ef5b2e8b45c135294f03cb
Andrew Millman <andrewmillman35@gmail.com>
2015-02-21 20:50:59 -0800

    Add constants file

--- KABadges/KABadges/KABBadgeTableViewCell.m
+++ KABadges/KABadges/KABBadgeTableViewCell.m
@@ -7,6 +7,7 @@
 //
 
 #import "KABBadgeTableViewCell.h"
+#import "KABConstants.h"
 
 @implementation KABBadgeTableViewCell
 
---
+++ KABadges/KABadges/KABConstants.h
@@ -0,0 +1,12 @@
+//
+//  KABConstants.h
+//  KABadges
+//
+//  Created by Andrew on 2/21/15.
+//  Copyright (c) 2015 Andrew Millman. All rights reserved.
+//
+
+#define KABadges_KABConstants_h
+
+static const CGFloat STANDARD_MARGIN = 10.0f;
+static const CGFloat LABEL_CORNER_RADIUS = 5.0f;
