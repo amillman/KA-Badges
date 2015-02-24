7428efa96551deeb5b1d3e4420ef41509ece6321
Andrew Millman <andrewmillman35@gmail.com>
2015-02-24 14:50:11 -0800

    Add category collection view cell

---
+++ KABadges/KABadges/KABCategoryCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  KABCategoryCollectionViewCell.h
+//  KABadges
+//
+//  Created by Andrew on 2/24/15.
+//  Copyright (c) 2015 Andrew Millman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KABCategoryCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ KABadges/KABadges/KABCategoryCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  KABCategoryCollectionViewCell.m
+//  KABadges
+//
+//  Created by Andrew on 2/24/15.
+//  Copyright (c) 2015 Andrew Millman. All rights reserved.
+//
+
+#import "KABCategoryCollectionViewCell.h"
+
+@implementation KABCategoryCollectionViewCell
+
+@end
